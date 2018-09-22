85886d2c867e6dfabea7cd5bda207e9534ae082b
ebikei <ebikei@hotmail.com>
2018-09-21 17:45:28 -0700

    Update PhysicalTest_FirstCode.R

--- Code/PhysicalTest_FirstCode.R
+++ Code/PhysicalTest_FirstCode.R
@@ -1,44 +1,128 @@
-x=c('tidyverse', 'foreign')
+x=c('tidyverse', 'foreign', 'readr')
 lapply(x,require,character.only=T)
 options(scipen=999)
 
-SchoolLocation = read.dbf('C:\\Users\\kebisu\\Documents\\Research\\UCDavisEnvJust\\Data\\GIS\\pubschls.dbf', as.is = TRUE) %>%
-	filter(DOCType != 'State Special Schools') %>%
-	select(CDSCode, School, Street, City, State, Zip, DOCType, SOCType, Latitude, Longitude) %>%
-	filter(!is.na(Latitude))
-
-SchoolName = read.csv('C:\\Users\\kebisu\\Documents\\Research\\UCDavisEnvJust\\Data\\PhysicalTest\\Entities2006.txt', stringsAsFactors=FALSE) %>%
-	mutate(SchoolID = paste0(str_sub(paste0('0', cds_code), -14, -1), str_sub(paste0('00', CharterNum), -3, -1))) %>%
-	distinct(SchoolID, .keep_all = TRUE) %>%
-	select(-cds_code, -CharterNum, -Scode, -Dcode, -Ccode)
-
-exam.2006 = read.csv('C:\\Users\\kebisu\\Documents\\Research\\UCDavisEnvJust\\Data\\PhysicalTest\\PhysFit2006.txt', stringsAsFactors=FALSE) %>%
-	mutate(Type = paste0(SubGrp, RptType, line_num), SchoolID = paste0(str_sub(paste0('0', cds_code), -14, -1), str_sub(paste0('00', charternum), -3, -1)))  %>%
-	mutate_each_(funs(as.numeric), c("Gr05_Stu","Gr5PctIn","Gr5PctNotIn","Gr07_Stu","Gr7PctIn","Gr7PctNotIn","Gr09_Stu","Gr9PctIn","Gr9PctNotIn")) 
-
-df.2006 = exam.2006 %>%
-	filter(Level == 1, Type %in% c('011', '012', '013', '014', '015', '016')) %>%
-	select(Type, SchoolID, Gr05_Stu, Gr5PctIn, Gr5PctNotIn, Gr07_Stu, Gr7PctIn, Gr7PctNotIn, Gr09_Stu, Gr9PctIn, Gr9PctNotIn) %>%
-	inner_join(SchoolName, by = 'SchoolID') %>%
-	mutate(CDSCode = substr(SchoolID, 1, 14)) %>%
-	inner_join(SchoolLocation, by = 'CDSCode') %>%
-	filter(!is.na(Latitude))
-
-test= df.2006 %>% filter(Gr07_Stu > 10)
+SchoolLocation = read_delim("Research/UCDavisEnvJust/Data/PhysicalTest/SchoolLocation.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+	data.frame() %>%
+	mutate(CharterNum = ifelse( !is.na(CharterNum), CharterNum, '0000')) %>%
+	mutate(SchoolID = paste0(str_sub(paste0('0', CDSCode), -14, -1), str_sub(paste0('000', CharterNum), -4, -1))) %>%
+	select(cds_code = CDSCode, SchoolID, Street, City, State, Zip, DOCType, SOCType,  Latitude, Longitude) 
 
 
 df = data.frame()
-for (iii in 2001:2008){
+for (iii in 2001:2002){
+temp.school = read.csv(paste0('C:\\Users\\kebisu\\Documents\\Research\\UCDavisEnvJust\\Data\\PhysicalTest\\Entities', iii, '.txt'), stringsAsFactors=FALSE)  %>%
+	mutate(cds_code = str_sub(paste0('0', cds_code), -14, -1)) %>%
+	select(cds_code, County, District, School)
+
 temp = read.csv(paste0('C:\\Users\\kebisu\\Documents\\Research\\UCDavisEnvJust\\Data\\PhysicalTest\\PhysFit', iii, '.txt'), stringsAsFactors=FALSE) %>%
-	mutate(Type = paste0(SubGrp, RptType, line_num), SchoolID = paste0(str_sub(paste0('0', cds_code), -14, -1), str_sub(paste0('00', charternum), -3, -1)))  %>%
-	mutate_each_(funs(as.numeric), c("Gr05_Stu","Gr5PctIn","Gr5PctNotIn","Gr07_Stu","Gr7PctIn","Gr7PctNotIn","Gr09_Stu","Gr9PctIn","Gr9PctNotIn")) 
+	mutate(Type = paste0(SubGrp, RptType, line_num), cds_code = str_sub(paste0('0', cds_code), -14, -1)) %>%
+	filter(Level == 1, Type %in% c('011', '012', '013', '014', '015', '016')) %>%
+	inner_join(temp.school, by = 'cds_code') %>%
+	inner_join(SchoolLocation, by = 'cds_code') %>%
+	mutate(year = iii, SchoolID = paste0(cds_code, '0000')) %>%
+	select(cds_code, SchoolID, year, Type, Gr05_Stu, Gr5PctIn, Gr5PctNotIn, Gr07_Stu, Gr7PctIn, Gr7PctNotIn, Gr09_Stu, Gr9PctIn, Gr9PctNotIn, County, District, School, Street, City, State, Zip, DOCType, SOCType, Latitude, Longitude)
+
 df = rbind(df, temp)
-rm(temp)
+rm(temp, temp.school)
+}
+
+for (iii in 2003:2008){
+temp.school = read.csv(paste0('C:\\Users\\kebisu\\Documents\\Research\\UCDavisEnvJust\\Data\\PhysicalTest\\Entities', iii, '.txt'), stringsAsFactors=FALSE)  %>%
+	mutate(cds_code = str_sub(paste0('0', cds_code), -14, -1)) %>%
+	mutate(SchoolID = paste0(str_sub(paste0('0', cds_code), -14, -1), str_sub(paste0('000', CharterNum), -4, -1))) %>%
+	select(cds_code, SchoolID, County, District, School)
+
+temp = read.csv(paste0('C:\\Users\\kebisu\\Documents\\Research\\UCDavisEnvJust\\Data\\PhysicalTest\\PhysFit', iii, '.txt'), stringsAsFactors=FALSE) %>%
+	mutate(Type = paste0(SubGrp, RptType, line_num), cds_code = str_sub(paste0('0', cds_code), -14, -1)) %>%
+	mutate(SchoolID = paste0(str_sub(paste0('0', cds_code), -14, -1), str_sub(paste0('000', charternum), -4, -1))) %>%
+	filter(Level == 1, Type %in% c('011', '012', '013', '014', '015', '016')) %>%
+	select(-cds_code) %>%
+	inner_join(temp.school, by = 'SchoolID') %>%
+	select(-cds_code) %>%
+	inner_join(SchoolLocation, by = 'SchoolID') %>%
+	mutate(year = iii) %>%
+	select(cds_code, SchoolID, year, Type, Gr05_Stu, Gr5PctIn, Gr5PctNotIn, Gr07_Stu, Gr7PctIn, Gr7PctNotIn, Gr09_Stu, Gr9PctIn, Gr9PctNotIn, County, District, School, Street, City, State, Zip, DOCType, SOCType, Latitude, Longitude)
+
+df = rbind(df, temp)
+rm(temp, temp.school)
 }
 
 
-test2 = filter(temp, Level ==1, SubGrp == 0, RptType ==1, line_num ==1)
+####### Geocode for schools without lat/long
+test = distinct(df, SchoolID, .keep_all = TRUE) %>%
+	filter(is.na(Latitude)) %>%
+	arrange(SchoolID) 
 
-test2[duplicated(test2$cds_code),]
+test2 = mutate(test, place = paste(Street, City, State, sep = ', ')) %>%
+	select(place)
+try1 = mutate_geocode(test2, place)
+
+set1 = cbind(test, try1) %>%
+	mutate(Latitude = lat, Longitude = lon)
+
+new1 = filter(set1, !is.na(Latitude)) %>%
+	select(- c(place, lon, lat)) 
+
+test3 = filter(set1, is.na(Latitude)) 
+try2 = test3 %>%
+	select(place) %>%
+	mutate_geocode(place)
+
+new2 = test3 %>%
+	select( -c(place, lon, lat)) %>%
+	cbind(try2) %>%
+	mutate(Latitude = lat, Longitude = lon) %>%
+	filter(!is.na(Latitude)) %>%
+	select(- c(place, lon, lat)) 
+
+test4 = test3 %>%
+	select( -c(place, lon, lat)) %>%
+	cbind(try2) %>%
+	mutate(Latitude = lat, Longitude = lon) %>%
+	filter(is.na(Latitude)) 
+try3 = test4 %>%
+	select(place) %>%
+	mutate_geocode(place)
+
+new3 = test4 %>%
+	select( -c(place, lon, lat)) %>%
+	cbind(try3) %>%
+	mutate(Latitude = lat, Longitude = lon) %>%
+	filter(!is.na(Latitude)) %>%
+	select(- c(place, lon, lat)) 	
+
+test5 = test4 %>%
+	select( -c(place, lon, lat)) %>%
+	cbind(try3) %>%
+	mutate(Latitude = lat, Longitude = lon) %>%
+	filter(is.na(Latitude)) 
+try4 = test5 %>%
+	select(place) %>%
+	mutate_geocode(place)
+
+new4 = test5 %>%
+	select( -c(place, lon, lat)) %>%
+	cbind(try4) %>%
+	mutate(Latitude = lat, Longitude = lon) %>%
+	filter(!is.na(Latitude)) %>%
+	select(- c(place, lon, lat)) 	
+
+test6 = test5 %>%
+	select( -c(place, lon, lat)) %>%
+	cbind(try4) %>%
+	mutate(Latitude = lat, Longitude = lon) %>%
+	filter(is.na(Latitude)) 
+try5 = test6 %>%
+	select(place) %>%
+	mutate_geocode(place)
+
+new5 = test6 %>%
+	select( -c(place, lon, lat)) %>%
+	cbind(try5) %>%
+	mutate(Latitude = lat, Longitude = lon) %>%
+	filter(!is.na(Latitude)) %>%
+	select(- c(place, lon, lat)) 	
 
 #https://www.cde.ca.gov/ta/tg/pf/pftresearch.asp
+#https://www.cde.ca.gov/ds/si/ds/pubschls.asp
